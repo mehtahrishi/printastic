99266118d83fab2f857d086d201a840998f388bd
Hrishi Mehta <mehtahrishi45@gmail.com>
2025-12-16 20:04:48 +0000

    now slightly increase map container and map height

--- src/app/(public)/contact-us/page.tsx
+++ src/app/(public)/contact-us/page.tsx
@@ -313,7 +313,7 @@ export default function ContactUsPage() {
                         <iframe
                         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3770.0932570183004!2d72.99680527466573!3d19.10356435110773!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c156b7c2b7d7%3A0x29c68b4cd30ab0c7!2sHonesty%20Print%20House!5e0!3m2!1sen!2sin!4v1765914189428!5m2!1sen!2sin"
                         width="100%"
-                        height="250"
+                        height="300"
                         style={{ border: 0 }}
                         allowFullScreen={true}
                         loading="lazy"
@@ -337,5 +337,3 @@ export default function ContactUsPage() {
     </div>
   );
 }
-
-    
