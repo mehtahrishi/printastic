c51b804ebfc7aa06e3041fca54926991dd5b1dc8
Hrishi Mehta <mehtahrishi45@gmail.com>
2025-12-16 19:45:48 +0000

    no not thsi much long hiefght and width make it small

--- src/app/(public)/contact-us/page.tsx
+++ src/app/(public)/contact-us/page.tsx
@@ -316,11 +316,11 @@ export default function ContactUsPage() {
               </CardDescription>
             </CardHeader>
             <CardContent>
-              <div className="aspect-w-16 aspect-h-9 rounded-lg overflow-hidden border">
+              <div className="aspect-video rounded-lg overflow-hidden border">
                 <iframe
                   src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3770.0932570183004!2d72.99680527466573!3d19.10356435110773!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3be7c156b7c2b7d7%3A0x29c68b4cd30ab0c7!2sHonesty%20Print%20House!5e0!3m2!1sen!2sin!4v1765914189428!5m2!1sen!2sin"
                   width="100%"
-                  height="450"
+                  height="100%"
                   style={{ border: 0 }}
                   allowFullScreen={true}
                   loading="lazy"
