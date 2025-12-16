65e68958487d68ffd3d3ef1dc7d75f9939122558
Hrishi Mehta <mehtahrishi45@gmail.com>
2025-12-16 20:02:25 +0000

    <blockquote class="instagram-media" data-instgrm-captioned data-instgrm-

--- src/app/(public)/contact-us/page.tsx
+++ src/app/(public)/contact-us/page.tsx
@@ -203,10 +203,28 @@ export default function ContactUsPage() {
                       style={{ background: '#FFF', border: 0, borderRadius: '3px', boxShadow: '0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15)', margin: '1px', maxWidth: '100%', minWidth: '280px', padding: 0, width: 'calc(100% - 2px)' }}
                   >
                   </blockquote>
-                  <Script async src="//www.instagram.com/embed.js" />
               </div>
-              {/* Placeholder for the second reel */}
-              <div></div>
+              <div className="relative overflow-hidden" style={{ maxHeight: '400px' }}>
+                <blockquote
+                  className="instagram-media"
+                  data-instgrm-captioned
+                  data-instgrm-permalink="https://www.instagram.com/reel/DI02p_9ysbr/?utm_source=ig_embed&amp;utm_campaign=loading"
+                  data-instgrm-version="14"
+                  style={{
+                    background: '#FFF',
+                    border: 0,
+                    borderRadius: '3px',
+                    boxShadow: '0 0 1px 0 rgba(0,0,0,0.5),0 1px 10px 0 rgba(0,0,0,0.15)',
+                    margin: '1px',
+                    maxWidth: '540px',
+                    minWidth: '326px',
+                    padding: 0,
+                    width: 'calc(100% - 2px)',
+                  }}
+                >
+                </blockquote>
+              </div>
+              <Script async src="//www.instagram.com/embed.js" />
             </div>
           </div>
 
